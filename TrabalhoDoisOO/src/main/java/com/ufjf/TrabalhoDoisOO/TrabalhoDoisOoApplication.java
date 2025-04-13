6acd1f57353e72f846b7be5e464589fe6b47ee05
Caio Dev <caioinnocencio@gmail.com>
2025-04-13 19:23:58 -0300

    Criando o projeto

---
+++ TrabalhoDoisOO/src/main/java/com/ufjf/TrabalhoDoisOO/TrabalhoDoisOoApplication.java
@@ -0,0 +1,13 @@
+package com.ufjf.TrabalhoDoisOO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoDoisOoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoDoisOoApplication.class, args);
+	}
+
+}
---
+++ TrabalhoDoisOO/src/test/java/com/ufjf/TrabalhoDoisOO/TrabalhoDoisOoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufjf.TrabalhoDoisOO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoDoisOoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
